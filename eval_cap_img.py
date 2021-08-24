66a09977de0376dfeb0888ecf46cf6490ef79639
git <git@localhost>
2021-08-24 20:57:55 +0800

    update

--- eval_cap_img.py
+++ eval_cap_img.py
@@ -1,10 +1,10 @@
 import os
+import pickle
 
 import numpy as np
 import torch
 import torch.nn.functional as F
 import torch.utils.data
-from gensim.models import FastText
 from nltk.tokenize import sent_tokenize, word_tokenize
 
 import datasets
@@ -72,41 +72,6 @@ def create_data_from(list_of_words, vocab):
     return list_of_indexes
 
 
-def create_emb(vocab, model=None):
-    if model is None:
-        model = FastText(size=emb_size, window=len_window, min_count=min_count)
-        model.build_vocab(sentences=list_of_words_train)
-        model.train(sentences=list_of_words_train, total_examples=len(list_of_words_train), epochs=epochs)
-
-    i2w = vocab['i2w']
-    base = np.ones((emb_size,), dtype='float32')
-    emb = [base * (i - 1) for i in range(3)]
-    for word in list(i2w.values())[3:]:
-        emb.append(model.wv[word])
-
-    emb = torch.from_numpy(np.array(emb))
-    return emb
-
-
-def calc_weights(vocab, a=1e-3):
-    occ_register = OrderedCounter()
-    for words in list_of_words_train:
-        occ_register.update(words)
-
-    w2i = vocab['w2i']
-    weights = np.zeros(len(w2i), dtype='float32')
-    total_occ = sum(list(occ_register.values()))
-    exc_occ = 0
-    for w, occ in occ_register.items():
-        if w in w2i.keys():
-            weights[w2i[w]] = a / (a + occ / total_occ)
-        else:
-            exc_occ += occ
-    weights[0] = a / (a + exc_occ / total_occ)
-
-    return torch.from_numpy(weights)
-
-
 def apply_weights(word_emb, weights, data):
     def truncate(s):
         # np.where works for pytorch boolean tensor
@@ -126,39 +91,36 @@ def apply_pc(sent_emb, u):
     return torch.cat([e - torch.matmul(u, e.unsqueeze(-1)).squeeze() for e in sent_emb.split(2048, 0)])
 
 
-# import pickle
-# with open('/tmp/CCA_emb/cub.emb', 'rb') as file:
-#     emb = pickle.load(file)
-#     emb = torch.from_numpy(emb).float()
-#     print('loaded emb', emb.shape)
-#     print(emb[:3])
-#
-# with open('/tmp/CCA_emb/cub.weights', 'rb') as file:
-#     weights = pickle.load(file)
-#     weights = torch.from_numpy(weights).float()
-#     print('loaded emb')
-#
-# with open('/tmp/CCA_emb/cub.pc', 'rb') as file:
-#     u = pickle.load(file)
-#     u = u.float()
-#     print('loaded u')
+def word2index(list_of_words, vocab):
+    list_of_indexes = []
+    for words in list_of_words:
+        list_of_indexes.append([vocab['w2i'].get(w, vocab['w2i'][sym_exc]) for w in words])
+    return torch.from_numpy(np.array(list_of_indexes)).int()
 
-# print('weights:', weights.min(), weights.max(), weights.mean())
-# print('emb:', emb.shape, emb.min(), emb.max(), emb.mean())
-# print('u:', u.min(), u.max(), u.mean())
 
-# img_mean = torch.load('/tmp/CCA_emb/images_mean.pt', map_location='cpu')
-# img_proj = torch.load('/tmp/CCA_emb/im_proj.pt', map_location='cpu')
-# cap_mean = torch.load('/tmp/CCA_emb/emb_mean.pt', map_location='cpu')
-# cap_proj = torch.load('/tmp/CCA_emb/emb_proj.pt', map_location='cpu')
-#
-# diff = img_emb.mean(dim=0) - img_mean
-# print('diff:', diff.sum(), diff.mean())
-# print(torch.allclose(img_emb.mean(dim=0), img_mean))
+# see also https://openreview.net/forum?id=SyK00v5xx
+def index2emb(x, embeddings, weights, u):
+    return apply_pc(apply_weights(embeddings, weights, x), u)
 
-# diff = cap_emb.mean(dim=0) - cap_mean
-# print('diff:', diff.sum(), diff.mean())
-# print(torch.allclose(cap_emb.mean(dim=0), cap_mean))
+
+def calculate_corr(captions, images, cap_mean, cap_proj, img_mean, img_proj):
+    return F.cosine_similarity((captions - cap_mean) @ cap_proj, (images - img_mean) @ img_proj).mean()
+
+
+with open(os.path.join(opt.dataroot, 'CCA_emb/cub.emb'), 'rb') as file:
+    embeddings = pickle.load(file)
+    embeddings = torch.from_numpy(embeddings).float()
+    print('loaded emb')
+
+with open(os.path.join(opt.dataroot, 'CCA_emb/cub.weights'), 'rb') as file:
+    weights = pickle.load(file)
+    weights = torch.from_numpy(weights).float()
+    print('loaded weights')
+
+with open(os.path.join(opt.dataroot, 'CCA_emb/cub.pc'), 'rb') as file:
+    u = pickle.load(file)
+    u = u.float()
+    print('loaded u')
 
 with open(raw_data_train_path, 'r') as file:
     sentences_train = sent_tokenize(file.read())
@@ -167,6 +129,8 @@ list_of_words_train = [word_tokenize(line) for line in sentences_train]
 with open(raw_data_test_path, 'r') as file:
     sentences_test = sent_tokenize(file.read())
 list_of_words_test = [word_tokenize(line) for line in sentences_test]
+vocabulary = create_vocab_from(list_of_words_train)
+data_test = torch.from_numpy(np.array(create_data_from(list_of_words_test, vocabulary))).int()
 
 x1_dataset = datasets.CUBCaptionVector(opt.dataroot, split='test', normalization='min-max')
 x2_dataset = datasets.CUBImageFeature(opt.dataroot, split='test', normalization='min-max')
@@ -178,51 +142,24 @@ paired_dataloader = torch.utils.data.DataLoader(paired_dataset,
                                                 drop_last=False,
                                                 pin_memory=True)
 
-vocabulary = create_vocab_from(list_of_words_train)
-data_test = torch.from_numpy(np.array(create_data_from(list_of_words_test, vocabulary))).int()
-
-embeddings = create_emb(vocabulary, x1_dataset.model)
-weights = calc_weights(vocabulary)
-emb_dataset = apply_weights(embeddings, weights, data_test)
-_, _, V = torch.svd(emb_dataset - emb_dataset.mean(dim=0), some=True)
-v = V[:, 0].unsqueeze(-1)
-u = v.mm(v.t())
-
-
-def word2index(list_of_words):
-    list_of_indexes = []
-    for words in list_of_words:
-        list_of_indexes.append([vocabulary['w2i'].get(w, vocabulary['w2i'][sym_exc]) for w in words])
-    return torch.from_numpy(np.array(list_of_indexes)).int()
-
-
-# see also https://openreview.net/forum?id=SyK00v5xx
-def index2emb(x):
-    return apply_pc(apply_weights(embeddings, weights, x), u)
-
-
-def calculate_corr(captions, images):
-    return F.cosine_similarity((captions - cap_mean) @ cap_proj, (images - img_mean) @ img_proj).mean()
-
-
-cap_emb = index2emb(data_test)
+cap_emb = index2emb(data_test, embeddings, weights, u)
 img_emb = torch.repeat_interleave(x2_dataset.decode(x2_dataset.data), 10, dim=0)
-
 cap_mean = cap_emb.mean(dim=0, keepdim=True)
 img_mean = img_emb.mean(dim=0, keepdim=True)
 
 corr, (cap_proj, img_proj) = utils.cca([cap_emb, img_emb], k=40)
-groundtruth = calculate_corr(cap_emb, img_emb)
+
+groundtruth = calculate_corr(cap_emb, img_emb, cap_mean, cap_proj, img_mean, img_proj)
 print('Largest eigen value from CCA: {:.3f}'.format(corr[0]))
 print('gt corr:', groundtruth)
 
-cap_mean = cap_mean.to(device)
-img_mean = img_mean.to(device)
-cap_proj = cap_proj.to(device)
-img_proj = img_proj.to(device)
 embeddings = embeddings.to(device)
 weights = weights.to(device)
 u = u.to(device)
+cap_mean = cap_mean.to(device)
+cap_proj = cap_proj.to(device)
+img_mean = img_mean.to(device)
+img_proj = img_proj.to(device)
 
 
 def calc_cross_coherence(loader, cap_encoder, cap_decoder, img_encoder, img_decoder):
@@ -243,13 +180,13 @@ def calc_cross_coherence(loader, cap_encoder, cap_decoder, img_encoder, img_deco
         img2cap.append(dec_x1)
         cap2img.append(dec_x2)
 
-    cap = index2emb(word2index(x1_dataset.decode(torch.cat(cap, dim=0))))
+    cap = index2emb(word2index(x1_dataset.decode(torch.cat(cap, dim=0)), vocabulary), embeddings, weights, u)
     img = x2_dataset.decode(torch.cat(img, dim=0))
     cap2img = x2_dataset.decode(torch.cat(cap2img, dim=0))
-    img2cap = index2emb(word2index(x1_dataset.decode(torch.cat(img2cap, dim=0))))
+    img2cap = index2emb(word2index(x1_dataset.decode(torch.cat(img2cap, dim=0)), vocabulary), embeddings, weights, u)
 
-    score_cap2img = F.cosine_similarity((cap - cap_mean) @ cap_proj, (cap2img - img_mean) @ img_proj).mean()
-    score_img2cap = F.cosine_similarity((img2cap - cap_mean) @ cap_proj, (img - img_mean) @ img_proj).mean()
+    score_cap2img = calculate_corr(cap, cap2img, cap_mean, cap_proj, img_mean, img_proj)
+    score_img2cap = calculate_corr(img2cap, img, cap_mean, cap_proj, img_mean, img_proj)
     return score_cap2img, score_img2cap
 
 
@@ -259,10 +196,10 @@ def calc_joint_coherence(cap_decoder, img_decoder, total=10000):
     cap = cap_decoder(z)
     img = img_decoder(z)
 
-    cap = index2emb(word2index(x1_dataset.decode(cap)))
+    cap = index2emb(word2index(x1_dataset.decode(cap), vocabulary), embeddings, weights, u)
     img = x2_dataset.decode(img)
 
-    score = F.cosine_similarity((cap - cap_mean) @ cap_proj, (img - img_mean) @ img_proj).mean()
+    score = calculate_corr(cap, img, cap_mean, cap_proj, img_mean, img_proj)
     return score
 
 
@@ -290,13 +227,13 @@ def calc_synergy_coherence(loader, cap_encoder, cap_decoder, img_encoder, img_de
         cap2cap.append(dec_x1)
         img2img.append(dec_x2)
 
-    cap = index2emb(word2index(x1_dataset.decode(torch.cat(cap, dim=0))))
+    cap = index2emb(word2index(x1_dataset.decode(torch.cat(cap, dim=0)), vocabulary), embeddings, weights, u)
     img = x2_dataset.decode(torch.cat(img, dim=0))
     img2img = x2_dataset.decode(torch.cat(img2img, dim=0))
-    cap2cap = index2emb(word2index(x1_dataset.decode(torch.cat(cap2cap, dim=0))))
+    cap2cap = index2emb(word2index(x1_dataset.decode(torch.cat(cap2cap, dim=0)), vocabulary), embeddings, weights, u)
 
-    score_cap2cap = F.cosine_similarity((cap - cap_mean) @ cap_proj, (img2img - img_mean) @ img_proj).mean()
-    score_img2img = F.cosine_similarity((cap2cap - cap_mean) @ cap_proj, (img - img_mean) @ img_proj).mean()
+    score_cap2cap = calculate_corr(cap2cap, img, cap_mean, cap_proj, img_mean, img_proj)
+    score_img2img = calculate_corr(cap, img2img, cap_mean, cap_proj, img_mean, img_proj)
     return score_cap2cap, score_img2img
 
 
